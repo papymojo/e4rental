c34969a5bb0c8df865d99df156aa1724c72ee7ed
formation <formation@formation-Aspire-X3995>
2017-10-10 17:47:50 +0200

    ajout des projet ui et eap

---
+++ com.sii.rental.ui/src/com/sii/rental/ui/views/RentalPart.java
@@ -0,0 +1,20 @@
+
+package com.sii.rental.ui.views;
+
+import javax.annotation.PostConstruct;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.e4.ui.di.Focus;
+
+public class RentalPart {
+
+	@PostConstruct
+	public void createContent(Composite parent) {
+		
+	}
+
+	@Focus
+	public void onFocus() {
+		
+	}
+
+}
