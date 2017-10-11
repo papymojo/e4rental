a5005e09e6b3fbfc404ce846f132d89dff458f3f
formation <formation@formation-Aspire-X3995>
2017-10-11 16:36:35 +0200

    Color !

--- com.sii.rental.ui/src/com/sii/rental/ui/AgencyLabelProvider.java
+++ com.sii.rental.ui/src/com/sii/rental/ui/AgencyLabelProvider.java
@@ -1,14 +1,18 @@
 package com.sii.rental.ui;
 
+import org.eclipse.jface.viewers.IColorProvider;
 import org.eclipse.jface.viewers.LabelProvider;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Color;
 import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.widgets.Display;
 
 import com.opcoach.training.rental.Customer;
 import com.opcoach.training.rental.Rental;
 import com.opcoach.training.rental.RentalAgency;
 import com.opcoach.training.rental.RentalObject;
 
-public class AgencyLabelProvider extends LabelProvider {
+public class AgencyLabelProvider extends LabelProvider implements IColorProvider{
 
 	
 	@Override
@@ -29,6 +33,26 @@ public String getText(Object element) {
 		return ((Rental) element).toString();
 	// TODO Auto-generated method stub
 	return super.getText(element);
+}
+
+@Override
+public Color getForeground(Object element) {
+	if (element instanceof Customer) {
+		return Display.getCurrent().getSystemColor(SWT.COLOR_BLUE);
+	} else if (element instanceof Rental) {
+		return Display.getCurrent().getSystemColor(SWT.COLOR_RED);
+	} else if (element instanceof RentalObject) {
+		return Display.getCurrent().getSystemColor(SWT.COLOR_GREEN);
+	} else if (element instanceof RentalAgency) {
+		return Display.getCurrent().getSystemColor(SWT.COLOR_MAGENTA);
+	}
+	return null;
+}
+
+@Override
+public Color getBackground(Object element) {
+	// TODO Auto-generated method stub
+	return null;
 }
 
 }
