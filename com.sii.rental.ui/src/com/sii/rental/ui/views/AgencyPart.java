916d3b868934b0d3b535346440a44e6804d6b2c8
formation <formation@formation-Aspire-X3995>
2017-10-11 17:50:07 +0200

    fix

--- com.sii.rental.ui/src/com/sii/rental/ui/views/AgencyPart.java
+++ com.sii.rental.ui/src/com/sii/rental/ui/views/AgencyPart.java
@@ -8,6 +8,10 @@ import javax.annotation.PostConstruct;
 
 import org.eclipse.e4.core.contexts.ContextInjectionFactory;
 import org.eclipse.e4.core.contexts.IEclipseContext;
+import org.eclipse.e4.ui.workbench.modeling.ESelectionService;
+import org.eclipse.jface.viewers.ISelectionChangedListener;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.viewers.SelectionChangedEvent;
 import org.eclipse.jface.viewers.TreeViewer;
 import org.eclipse.swt.widgets.Composite;
 
@@ -18,7 +22,7 @@ import com.sii.rental.ui.AgencyTreeProvider;
 public class AgencyPart {
 
 	@PostConstruct
-	public void postConstruct(Composite parent,RentalAgency a, IEclipseContext ctx) {
+	public void postConstruct(Composite parent,RentalAgency a,ESelectionService esel, IEclipseContext ctx) {
 		TreeViewer tv = new TreeViewer(parent);
 		tv.setContentProvider(new AgencyTreeProvider());
 		AgencyLabelProvider p = ContextInjectionFactory.make(AgencyLabelProvider.class,ctx);
@@ -26,6 +30,14 @@ public class AgencyPart {
 		Collection<RentalAgency> datas = new ArrayList<>();
 		datas.add(a);
 		tv.setInput(datas);
+		tv.addSelectionChangedListener(new ISelectionChangedListener() {
+			
+			@Override
+			public void selectionChanged(SelectionChangedEvent event) {
+				IStructuredSelection s = event.getStructuredSelection();
+				esel.setSelection((s.size() == 1)?s.getFirstElement():s.toArray());
+			}
+		});
 		tv.expandAll();
 	}
 
--- com.sii.rental.ui/src/com/sii/rental/ui/views/RentalPart.java
+++ com.sii.rental.ui/src/com/sii/rental/ui/views/RentalPart.java
@@ -1,8 +1,12 @@
 package com.sii.rental.ui.views;
 
 import javax.annotation.PostConstruct;
+import javax.inject.Inject;
+import javax.inject.Named;
 
+import org.eclipse.e4.core.di.annotations.Optional;
 import org.eclipse.e4.ui.di.Focus;
+import org.eclipse.e4.ui.services.IServiceConstants;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
@@ -12,7 +16,6 @@ import org.eclipse.swt.widgets.Label;
 
 import com.opcoach.training.rental.Rental;
 import com.opcoach.training.rental.RentalAgency;
-import com.sii.rental.core.RentalCoreActivator;
 
 public class RentalPart {
 
@@ -21,18 +24,19 @@ public class RentalPart {
 	private Label dateDebut;
 	private Label client;
 
-	@PostConstruct
-	public void createContent(Composite parent,RentalAgency a) {
+	@Inject
+	public RentalPart(Composite parent,RentalAgency a) {
 		parent.setLayout(new GridLayout(1, false));
 
 		Group infoGroup = new Group(parent, SWT.NONE);
-		infoGroup.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, false, false, 1, 1));
+		infoGroup.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
 		infoGroup.setText("Information");
 		infoGroup.setLayout(new GridLayout(3, false));
 		new Label(infoGroup, SWT.NONE);
 
 		rentedObjectLabel = new Label(infoGroup, SWT.BORDER);
 		GridData gd = new GridData();
+		gd.grabExcessHorizontalSpace = true;
 		gd.horizontalSpan = 2;
 		gd.horizontalAlignment = SWT.FILL;
 		rentedObjectLabel.setLayoutData(gd);
@@ -42,24 +46,26 @@ public class RentalPart {
 
 		client = new Label(infoGroup, SWT.NONE);
 		client.setText("New Label");
+		new Label(infoGroup, SWT.NONE);
 
 		Group datesGroup = new Group(parent, SWT.NONE);
 		datesGroup.setLayout(new GridLayout(2, false));
-		datesGroup.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, false, false, 1, 1));
+		datesGroup.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
 		datesGroup.setText("Dates de locations");
 
 		Label du = new Label(datesGroup, SWT.NONE);
 		du.setText("du :");
 
 		dateDebut = new Label(datesGroup, SWT.NONE);
+		dateDebut.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
 		dateDebut.setText("New Label");
 
 		Label au = new Label(datesGroup, SWT.NONE);
 		au.setText("au :");
 
 		dateFin = new Label(datesGroup, SWT.NONE);
+		dateFin.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
 		dateFin.setText("New Label");
-		setRental(a.getRentals().get(0));
 	}
 
 	@Focus
@@ -68,9 +74,23 @@ public class RentalPart {
 	}
 
 	public void setRental(Rental r) {
-		rentedObjectLabel.setText(r.getRentedObject().getName());
-		client.setText(r.getCustomer().getFirstName()+ " " +r.getCustomer().getLastName());
-		dateDebut.setText(r.getStartDate().toString());
-		dateFin.setText(r.getEndDate().toString());
+		if (r != null) {
+			rentedObjectLabel.setText(r.getRentedObject().getName());
+			client.setText(r.getCustomer().getFirstName()+ " " +r.getCustomer().getLastName());
+			dateDebut.setText(r.getStartDate().toString());
+			dateFin.setText(r.getEndDate().toString());
+		} else {
+			rentedObjectLabel.setText("");
+			client.setText("");
+			dateDebut.setText("");
+			dateFin.setText("");	
+		}
+
 	}
-}
+	
+	@Inject 
+	@Optional 
+	public void listenToSel(@Named(IServiceConstants.ACTIVE_SELECTION) Rental r) {
+		setRental(r);
+	}
+	}
